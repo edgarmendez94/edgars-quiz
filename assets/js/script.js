58d976acd117333a6181cfee975a208819188a26
Edgar Mendez <edgar.ym26@gmail.com>
2022-05-17 00:14:05 -0400

    added comments that should help me

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+function startQuiz(){
+    // start button goes away 
+    // A B C D buttons display
+    // time starts & displays countdown
+    // quiz header now turns to question
+    // a question is picked from list of Q's
+    // there is text next to A B C D buttons for answer
+}
+
+// event listener for click 
+// if click on correct button for Q , display next Q
+// else time is deducted if wrong answer
+
+// something needs to check when questions are all completed
+
+// something needs to check time left
+// if time over , game over 
+// ask for initials
+
+
+// scoreboard has list for initials 
+// scoreboard displays how much time was left for initial
+// scoreboard displays how many u got right & wrong
